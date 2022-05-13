44d2ff7ffedf7994fe11b3f42f89365c7e881654
anjana <anjana.someshwar@gmail.com>
2022-05-13 09:27:10 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AndroidApp()
+	{
+		
+		System.out.println("AndroidApp");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+
+
+}
---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void RestAPI()
+	{
+		
+		System.out.println("RestAPI");
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+	
+}
